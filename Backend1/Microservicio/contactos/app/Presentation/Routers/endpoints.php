18480c1dbaa59c9e53757ba89db0b9af49c5c626
HectorRosas76 <hecrosas@uniboyaca.edu.co>
2026-05-23 05:33:00 -0500

    arreglo

--- Backend1/Microservicio/contactos/app/Presentation/Routers/endpoints.php
+++ Backend1/Microservicio/contactos/app/Presentation/Routers/endpoints.php
@@ -11,30 +11,30 @@ return function (App $app) {
     $app->group('/api', function (RouteCollectorProxy $group) {
 
  
-        $group->group('/vehiculos', function (RouteCollectorProxy $veh) {
-            $veh->get('', [VehiculoRepository::class, 'list']);
-            $veh->get('/{id}', [VehiculoRepository::class, 'detail']);
-            $veh->post('', [VehiculoRepository::class, 'create']);
-            $veh->put('/{id}', [VehiculoRepository::class, 'update']);
-            $veh->delete('/{id}', [VehiculoRepository::class, 'delete']);
+        $group->group('/vehiculos', function (RouteCollectorProxy $group) {
+            $group->get('', [VehiculoRepository::class, 'list']);
+            $group->get('/{id}', [VehiculoRepository::class, 'detail']);
+            $group->post('', [VehiculoRepository::class, 'create']);
+            $group->put('/{id}', [VehiculoRepository::class, 'update']);
+            $group->delete('/{id}', [VehiculoRepository::class, 'delete']);
         });
 
        
-        $group->group('/contactos', function (RouteCollectorProxy $cli) {
-            $cli->get('', [ContactosRepository::class, 'list']);
-            $cli->get('/{id}', [ContactosRepository::class, 'detail']);
-            $cli->post('', [ContactosRepository::class, 'create']);
-            $cli->put('/{id}', [ContactosRepository::class, 'update']);
-            $cli->delete('/{id}', [ContactosRepository::class, 'delete']);
+        $group->group('/contactos', function (RouteCollectorProxy $group) {
+            $group->get('', [ContactosRepository::class, 'list']);
+            $group->get('/{id}', [ContactosRepository::class, 'detail']);
+            $group->post('', [ContactosRepository::class, 'create']);
+            $group->put('/{id}', [ContactosRepository::class, 'update']);
+            $group->delete('/{id}', [ContactosRepository::class, 'delete']);
         });
 
        
-        $group->group('/reservas', function (RouteCollectorProxy $res) {
-            $res->get('', [ReservaRepository::class, 'list']);
-            $res->get('/{id}', [ReservaRepository::class, 'detail']);
-            $res->post('', [ReservaRepository::class, 'create']);
-            $res->put('/{id}', [ReservaRepository::class, 'update']);
-            $res->delete('/{id}', [ReservaRepository::class, 'delete']);
+        $group->group('/reservas', function (RouteCollectorProxy $group) {
+            $group->get('', [ReservaRepository::class, 'list']);
+            $group->get('/{id}', [ReservaRepository::class, 'detail']);
+            $group->post('', [ReservaRepository::class, 'create']);
+            $group->put('/{id}', [ReservaRepository::class, 'update']);
+            $group->delete('/{id}', [ReservaRepository::class, 'delete']);
         });
 
     });
